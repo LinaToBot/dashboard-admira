66c1e809c16ac241308ccf50907e7698669d7c2a
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2024-08-11 01:53:40 -0600

    clean code

--- src/components/features/Dashboard/DemographyPieChart/DemographyPieChart.tsx
+++ src/components/features/Dashboard/DemographyPieChart/DemographyPieChart.tsx
@@ -25,12 +25,12 @@ export const DemographyPieChart: React.FC = () => {
   const ages = useAppSelector(selectDemographicAge);
   const genders = useAppSelector(selectDemographicGender);
 
-  const data01 = ages.map((ageRanges, index) => ({
+  const data01 = ages.map((ageRanges) => ({
     name: ageRanges.rango,
     value: ageRanges.porcentaje,
   }));
 
-  const data02 = genders.map((genderType, index) => ({
+  const data02 = genders.map((genderType) => ({
     name: genderType.tipo,
     value: genderType.porcentaje,
   }));
